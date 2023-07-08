c8f8cbc1eb2fe53f6afb4f50d8c9aea5c520d57f
Aganepes Ahmedow <82721519+aganepes@users.noreply.github.com>
2023-07-08 13:53:24 +0500

    Add files via upload

---
+++ Scrool animation/script.js
@@ -0,0 +1,15 @@
+const contianer=document.querySelector('.contianer');
+const boxs=document.querySelectorAll('.box')
+
+let boxAnimation=()=>{
+    let windowTop=window.innerHeight*0.8;
+    boxs.forEach((item)=>{
+        if(windowTop>item.getBoundingClientRect().top){
+            item.classList.add('show')
+        }else{
+            item.classList.remove('show')
+        }
+    })
+}
+window.addEventListener('scroll', boxAnimation);
+boxAnimation()
