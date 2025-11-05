c3a593f9743758c3af2103426c58650980c417c4
Aganepes <aaganepes@gmail.com>
2025-11-05 16:21:53 +0500

    change button ripple effect project

--- Button Ripple Efect/script.js
+++ Button Ripple Efect/script.js
@@ -1,11 +1,15 @@
 const Button=document.querySelector('button'),
-      span = document.querySelector('span');
+    span = document.querySelector('span');
       
 Button.addEventListener('click',(event)=>{
-const circle=document.createElement('span');
+    const circle=document.createElement('span');
     circle.classList.add('circle');
+
     circle.style.top=`${event.offsetY}px`;
     circle.style.left=`${event.offsetX}px`;
+
     Button.appendChild(circle);
-    setTimeout(()=>circle.remove(),1000);
-})
+
+    setTimeout(()=>circle.remove(),300);
+    
+});
